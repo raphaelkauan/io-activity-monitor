eac2afa5c6d5527f58ae57ea79da43d5ffecff5c
raphaelkauan <raphaelkauanoficial@gmail.com>
2025-04-22 23:33:41 -0400

    fix: correcting 10 days off metrics collection

--- src/commands/presence/metricsMember.ts
+++ src/commands/presence/metricsMember.ts
@@ -9,52 +9,69 @@ export default new Command({
   type: ApplicationCommandType.ChatInput,
 
   async run({ interaction }) {
-    if (!validationAdmin(interaction)) return;
+    if (!(await validationAdmin(interaction))) return;
 
-    const membersAtivoFilter = await Prisma.member.findMany({
-      where: { status: { equals: "ativo" } },
-      select: { username: true, globalName: true, status: true },
-    });
+    try {
+      const membersAtivoFilter = await Prisma.member.findMany({
+        where: { status: { equals: "ativo" } },
+        select: { username: true, globalName: true, status: true },
+      });
 
-    const metricsMembersAtivo = membersAtivoFilter.map(
-      (member) => `
-      • Nome: **${member.username}** NomeGlobal: **${member.globalName}** Status: **${member.status}**`
-    );
+      const metricsMembersAtivo = membersAtivoFilter.map(
+        (member) => `
+        • Nome: **${member.username}** NomeGlobal: **${member.globalName}** Status: **${member.status}**`
+      );
 
-    // 10 dias atrás
-    const today = new Date();
-    const tenDaysBefore = new Date();
-    tenDaysBefore.setDate(today.getDate() - 10);
+      // 10 dias atrás
+      const today = new Date();
+      const tenDaysBefore = new Date();
+      tenDaysBefore.setDate(today.getDate() - 10);
 
-    // 30 dias atrás
-    const thirtyDayBefore = new Date();
-    thirtyDayBefore.setDate(today.getDate() - 29);
+      console.log("10 " + tenDaysBefore);
 
-    const membersTenDayOff = await Prisma.member.findMany({
-      where: { lastOffline: { gt: tenDaysBefore, lte: thirtyDayBefore } },
-      select: { username: true, globalName: true, lastOffline: true, status: true },
-    });
+      // 30 dias atrás
+      const thirtyDayBefore = new Date();
+      thirtyDayBefore.setDate(today.getDate() - 30);
 
-    const metricsMembersTenDayOff = membersTenDayOff.map((member) => {
-      const formatDate = new Date(member.lastOffline!).toLocaleDateString("pt-BR");
-      return `
-      • Nome: **${member.username}** NomeGlobal: **${member.globalName}** Visto por último: **${formatDate}** Status: **${member.status}**`;
-    });
+      console.log("30 " + thirtyDayBefore);
 
-    const embed = new EmbedBuilder()
-      .setTitle(`Métricas **${interaction.guild?.name}**`)
-      .setColor("DarkAqua")
-      .setFields([
-        {
-          name: "**Membros +10 dias off**",
-          value: `${metricsMembersTenDayOff}`,
-        },
-        {
-          name: "**Membros ativos:**",
-          value: `${metricsMembersAtivo}`,
-        },
-      ]);
+      // Menos de 30 dias e mais de 10 dias
+      const membersTenDayOff = await Prisma.member.findMany({
+        where: { lastOffline: { gt: thirtyDayBefore, lte: tenDaysBefore } },
+        select: { username: true, globalName: true, lastOffline: true, status: true },
+      });
 
-    await interaction.reply({ embeds: [embed], flags: MessageFlags.Ephemeral });
+      console.log(membersTenDayOff);
+
+      const metricsMembersTenDayOff = membersTenDayOff.map((member) => {
+        const formatDate = new Date(member.lastOffline!).toLocaleDateString();
+        const format = `\n • Nome: **${member.username}** NomeGlobal: **${member.globalName}** Visto por último: **${formatDate}** Status: **${member.status}**`;
+        return format;
+      });
+
+      let messageTenDayOff: string[] = ["Não existe um membro off +10 dias"];
+
+      if (metricsMembersTenDayOff.length !== 0) {
+        messageTenDayOff = metricsMembersTenDayOff;
+      }
+
+      const embed = new EmbedBuilder()
+        .setTitle(`Métricas **${interaction.guild?.name}**`)
+        .setColor("DarkAqua")
+        .setFields([
+          {
+            name: "**Membros +10 dias off**",
+            value: `${messageTenDayOff}`,
+          },
+          {
+            name: "**Membros ativos:**",
+            value: `${metricsMembersAtivo}`,
+          },
+        ]);
+
+      await interaction.reply({ embeds: [embed], flags: MessageFlags.Ephemeral });
+    } catch (error) {
+      console.log(error);
+    }
   },
 });
--- src/commands/support/func/validation/validationAdmin.ts
+++ src/commands/support/func/validation/validationAdmin.ts
@@ -3,12 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export function validationAdmin(interaction: CommandInteraction) {
+export async function validationAdmin(interaction: CommandInteraction) {
   const cargoAdmId = process.env.CARGO_ADMIN_ID;
 
   if (interaction.member instanceof GuildMember) {
     if (!interaction.member?.roles.cache.has(cargoAdmId!)) {
-      interaction.reply({
+      await interaction.reply({
         content: `<@${interaction.user.id}> você não tem permissão para executar esse comando!`,
         flags: MessageFlags.Ephemeral,
       });
